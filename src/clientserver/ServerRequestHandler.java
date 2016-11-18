c7cbf5ec102b1ce6f3a762b0d41b4f1dbf2bd186
Tommy Schnabel <tommyschnabel@gmail.com>
2016-11-17 19:27:38 -0500

    Fixed bad merge

--- src/clientserver/ServerRequestHandler.java
+++ src/clientserver/ServerRequestHandler.java
@@ -1,79 +1,74 @@
 package clientserver;
 
-import stats.clientserver.ClientChapterPacketGetStats;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
-//test again to see if working james
+
 /**
  * Handles an incoming server request from a client.
  */
 public class ServerRequestHandler implements Runnable {
 
     private final Socket clientRequestSocket;
-    private final ClientChapterPacketGetStats stats;
-    private PrintWriter out; // writers (for writing back to the machine and to destination)
-    private BufferedReader in; // reader (for reading from the machine connected to)
-    private String ChapterRequested; // communication strings
 
-    public ServerRequestHandler(Socket clientRequestSocket, ClientChapterPacketGetStats stats) {
+    public ServerRequestHandler(Socket clientRequestSocket) {
         this.clientRequestSocket = clientRequestSocket;
-        this.stats = stats;
-
-      //(A) Receive a TCP connection,
-     
+    }
 
     @Override
     public void run() {
-        //TODO Handle incoming request here
-        // TODO: Reference the filestoserve folder created in ClientServer main()
-    	   try 
-       	{    	
-       	   out = new PrintWriter(clientRequestSocket.getOutputStream(), true);
+        OutputStream out;
+        BufferedReader in;// reader (for reading from the machine connected to)
+    	try {
+            //(A) Receive a TCP connection
+    	   out = clientRequestSocket.getOutputStream();
    		   in = new BufferedReader(new InputStreamReader(clientRequestSocket.getInputStream()));
-       
-   		}
-       	
-   		catch (IOException e) 
-       	{
-               System.err.println("Client failed to connect.");            
-           }
-       }
-    
+   		} catch (IOException e) {
+            System.err.println("FATAL: ServerRequestHandler: Client failed to connect: " + e.getMessage());
+            return;
+        }
+
     	// Initial sends/receives
-    	try
-		{
-	    		
-    	//(B) read the chapter number from that connection, 
-    		
-        ChapterRequested = in.readLine(); // Get requested chapter 
-		System.out.println("Getting Chapter: " + ChapterRequested);
-		out.println("Connected to the client seeking chapter"+ ChapterRequested ); // confirmation of connection
-		
-	    //wait 1 second 
-		try{
-    		Thread.currentThread();
-			Thread.sleep(1000); 
-	   }
-		catch(InterruptedException ie){
-		System.out.println("Thread interrupted");
-		}
+    	try {
 
-    	
+            //(B) read the chapter number from that connection,
+            String chapterRequested = in.readLine();
+            System.out.println("Getting Chapter: " + chapterRequested);
+            System.out.println("Connected to the client seeking chapter" + chapterRequested); // confirmation of connection
 
-    	
-    	//read the chapter from the file system, 
-		
-    	//send the contents of that chapter through the connection.
+            //read the chapter from the file system,
+            File file = new File("C:\\Users\\JamesLaptop\\Desktop\\ServerTest\\Chapter_2.txt");
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
 
-    }catch (IOException e) {
-        System.err.println("Could not listen to socket.");}
-        //System.exit(1);
-    	
-    	
-    	
-   }
+            //Read File Contents into contents array
+            byte[] contents;
+            long fileLength = file.length();
+            long current = 0;
+
+            long start = System.nanoTime();
+            while(current!=fileLength){
+                int size = 10000;
+                if(fileLength - current >= size) {
+                    current += size;
+                } else {
+                    size = (int)(fileLength - current);
+                    current = fileLength;
+                }
+                contents = new byte[size];
+                bis.read(contents, 0, size);
+                out.write(contents);
+                System.out.print("Sending file ... "+(current*100)/fileLength+"% complete!");
+            }
+
+            out.flush();
+            //File transfer done. Close the socket connection!
+            bis.close();
+            clientRequestSocket.close();   //JP Watch this for possible bug
+            System.out.println("File sent succesfully!");
+
+       } catch (IOException e) {
+            System.err.println("Could not listen to socket.");
+       }
+    }
 }
+
